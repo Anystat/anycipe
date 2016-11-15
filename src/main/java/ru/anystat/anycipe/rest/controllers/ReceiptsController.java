b3996e6b910c904214f7b5e2c967d8b0b261c327
ustits <ustitsruslan@gmail.com>
2016-11-15 22:48:42 +0300

    added construction for new controller

--- src/main/java/ru/anystat/anycipe/rest/controllers/ReceiptsController.java
+++ src/main/java/ru/anystat/anycipe/rest/controllers/ReceiptsController.java
@@ -1,8 +1,10 @@
 package ru.anystat.anycipe.rest.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.bson.Document;
+import org.springframework.web.bind.annotation.*;
 import ru.anystat.anycipe.rest.repositories.ReceiptsRepository;
+
+import java.util.Map;
 
 /**
  * Created by ustits on 09.11.16.
@@ -10,5 +12,11 @@ import ru.anystat.anycipe.rest.repositories.ReceiptsRepository;
 @RestController
 @RequestMapping("/receipts")
 public class ReceiptsController extends AbstractController<ReceiptsRepository> {
+
+    @RequestMapping(value = "/{name}/ingredients", method = RequestMethod.GET)
+    public Iterable<Document> getEntriesByMultipleParams(@PathVariable String name,
+                                                         @RequestParam Map<String, String> requestParams) {
+        return null;
+    }
 
 }
