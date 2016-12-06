03d30d4f3664dbeb95ab780aed441316e5bb23b0
ustits <ustitsruslan@gmail.com>
2016-12-06 23:36:00 +0300

    #5 added search of ingredients by category

--- src/main/java/ru/anystat/anycipe/rest/controllers/IngredientsController.java
+++ src/main/java/ru/anystat/anycipe/rest/controllers/IngredientsController.java
@@ -1,7 +1,12 @@
 package ru.anystat.anycipe.rest.controllers;
 
+import com.mongodb.client.FindIterable;
+import org.bson.Document;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.anystat.anycipe.rest.model.Receipt;
 import ru.anystat.anycipe.rest.repositories.IngredientsRepository;
 
 /**
@@ -11,4 +16,8 @@ import ru.anystat.anycipe.rest.repositories.IngredientsRepository;
 @RequestMapping("/ingredients")
 public class IngredientsController extends AbstractController<IngredientsRepository> {
 
+    @RequestMapping(value = "/category/{name}")
+    public FindIterable<Document> getEntriesByMultipleParamsInBody(@PathVariable String name) {
+        return getRepository().findByCategory(name);
+    }
 }
--- src/main/java/ru/anystat/anycipe/rest/repositories/IngredientsRepository.java
+++ src/main/java/ru/anystat/anycipe/rest/repositories/IngredientsRepository.java
@@ -1,5 +1,8 @@
 package ru.anystat.anycipe.rest.repositories;
 
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +14,16 @@ public class IngredientsRepository extends AbstractRepository {
 
     private static final String COLLECTION = "ingredients";
     private static final String INGREDIENT_NAME = "ingredient";
+    private static final String CATEGORY_FIELD_NAME = "category";
 
     public IngredientsRepository(@Value(COLLECTION) String collectionName,
                                  @Value(INGREDIENT_NAME) String searchField) {
         super(collectionName, searchField);
+    }
+
+    public FindIterable<Document> findByCategory(String category) {
+        return getDBContext()
+                .getCollection(COLLECTION)
+                .find(Filters.eq(CATEGORY_FIELD_NAME, category));
     }
 }
